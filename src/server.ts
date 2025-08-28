b318ac3c7e4b8b8f2e90d6bef23029ffc3d2ad84
shax201 <shahadatjaman228@gmail.com>
2025-08-28 13:29:13 +0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,12 @@ async function main() {
   try {
     const PORT = process.env.PORT || 5000;
     await mongoose.connect(config.database_url as string);
-    app.listen(PORT,'0.0.0.0', () => {
-      console.log(`Server is running on port http://localhost:${config.port}`);
+
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`Server is running on port http://0.0.0.0:${PORT}`);
     });
   } catch (error) {
-    console.log(error);
+    console.error('Error starting server:', error);
   }
 }
 
