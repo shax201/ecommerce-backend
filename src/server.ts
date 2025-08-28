e4c7be3eaef0badbdba882e6270943bbec782d16
shax201 <shahadatjaman228@gmail.com>
2025-08-28 13:18:54 +0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,9 @@ import { config } from './config';
 
 async function main() {
   try {
+    const PORT = process.env.PORT || 5000;
     await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
+    app.listen(PORT,'0.0.0.0', () => {
       console.log(`Server is running on port http://localhost:${config.port}`);
     });
   } catch (error) {
